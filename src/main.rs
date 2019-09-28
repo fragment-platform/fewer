095fd7e536342390d0bd97050da6e28213dcb0e3
Paul Miller <paul@pauljmiller.com>
2019-09-28 13:05:18 -0700

    fmt

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use std::collections::{HashSet, HashMap};
+use ammonia::Builder;
+use std::collections::{HashMap, HashSet};
 use std::env;
 use std::fs::File;
 use std::io::{self, Read, Write};
 use std::process;
-use ammonia::Builder;
 
 fn run() -> io::Result<()> {
     let input = env::args().nth(1).unwrap_or_else(|| String::from("-"));
@@ -21,65 +21,59 @@ fn run() -> io::Result<()> {
         Box::new(File::create(output)?)
     };
 
+    let mut tags = HashSet::new();
+    let mut tag_attributes = HashMap::new();
 
-     let mut tags = HashSet::new();
-     let mut tag_attributes = HashMap::new();
+    tags.insert("html");
+    tags.insert("head");
+    tags.insert("body");
+    tags.insert("title");
 
-     tags.insert("html");
-     tags.insert("head");
-     tags.insert("body");
-     tags.insert("title");
+    tags.insert("h1");
+    tags.insert("h2");
+    tags.insert("h3");
+    tags.insert("h4");
+    tags.insert("h5");
+    tags.insert("h6");
 
-     tags.insert("h1");
-     tags.insert("h2");
-     tags.insert("h3");
-     tags.insert("h4");
-     tags.insert("h5");
-     tags.insert("h6");
-     
-     tags.insert("p");
-     tags.insert("br");
+    tags.insert("p");
+    tags.insert("br");
 
-     tags.insert("a");
-     let mut a_attributes = HashSet::new();
-     a_attributes.insert("href");
-     a_attributes.insert("title");
-     tag_attributes.insert(
-         "a",
-         a_attributes
-     );
+    tags.insert("a");
+    let mut a_attributes = HashSet::new();
+    a_attributes.insert("href");
+    a_attributes.insert("title");
+    tag_attributes.insert("a", a_attributes);
 
-     tags.insert("img");
-     let mut img_attributes = HashSet::new();
-     img_attributes.insert("src");
-     img_attributes.insert("alt");
-     tag_attributes.insert(
-         "img",
-         img_attributes
-     );
+    tags.insert("img");
+    let mut img_attributes = HashSet::new();
+    img_attributes.insert("src");
+    img_attributes.insert("alt");
+    tag_attributes.insert("img", img_attributes);
 
-     tags.insert("strong");
-     tags.insert("em");
-     tags.insert("blockquote");
-     tags.insert("pre");
+    tags.insert("strong");
+    tags.insert("em");
+    tags.insert("blockquote");
+    tags.insert("pre");
 
+    tags.insert("ol");
+    tags.insert("ul");
+    tags.insert("li");
 
-     tags.insert("ol");
-     tags.insert("ul");
-     tags.insert("li");
+    tags.insert("hr");
 
-     tags.insert("hr");
+    tags.insert("table");
+    tags.insert("tr");
+    tags.insert("th");
+    tags.insert("td");
 
-     tags.insert("table");
-     tags.insert("tr");
-     tags.insert("th");
-     tags.insert("td");
+    Builder::new()
+        .tags(tags)
+        .tag_attributes(tag_attributes)
+        .clean_from_reader(&mut rdr)?
+        .write_to(&mut wrt)?;
 
-     Builder::new().tags(tags).tag_attributes(tag_attributes)
-         .clean_from_reader(&mut rdr)?
-         .write_to(&mut wrt)?;
-
-     Ok(())
+    Ok(())
 }
 
 fn main() {
